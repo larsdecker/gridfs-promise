2487c666c9c51276230c7b19fa75147bb9c7e011
Lars Decker <decker@ascora.de>
2018-01-16 17:18:07 +0100

    Add first structure

---
+++ src/GridFSPromise.js
@@ -0,0 +1,98 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var es6_promise_1 = require("es6-promise");
+var mongodb_1 = require("mongodb");
+var bson_1 = require("bson");
+var GridFSPromise = /** @class */ (function () {
+    /**
+     *
+     * @param {string} mongoUrl
+     * @param {string} databaseName
+     * @param {MongoClientOptions} mongoOptions
+     * @param {string} basePath
+     * @param {string} bucketName
+     */
+    function GridFSPromise(mongoUrl, databaseName, mongoOptions, basePath, bucketName) {
+        this.databaseName = databaseName;
+        this.connectionUrl = mongoUrl;
+        this.mongoClientOptions = mongoOptions;
+        this.basePath = basePath || __dirname;
+        this.bucketName = bucketName || "fs";
+    }
+    /**
+     * Save the File from the GridFs to the filesystem and get the Path back
+     * @param {string} id
+     * @param {string} fileName
+     * @param filePath
+     * @return {Promise<string>}
+     */
+    GridFSPromise.prototype.getFile = function (id, fileName, filePath) {
+        var _this = this;
+        if (filePath === void 0) { filePath = "/tmpArchive/archive/"; }
+        return new es6_promise_1.Promise(function (resolve, reject) {
+            var gridSave = mongodb_1.MongoClient.connect(_this.connectionUrl);
+            gridSave.then(function (client) {
+                var connection = client.db(_this.databaseName);
+                var bucket = new mongodb_1.GridFSBucket(connection, { bucketName: _this.bucketName });
+                bucket.openDownloadStream(new bson_1.ObjectID(id))
+                    .once("error", function (error) {
+                    reject(error);
+                }).once("finish", function () {
+                    resolve(_this.basePath.concat("" + filePath + fileName));
+                })
+                    .pipe(fs.createWriteStream(_this.basePath.concat("" + filePath + fileName)));
+            }).catch(function (err) {
+                reject(err);
+            });
+        });
+    };
+    /**
+     * Returns a stream of a file from the GridFS.
+     * @param {string} id
+     * @return {Promise<GridFSBucketReadStream>}
+     */
+    GridFSPromise.prototype.getFileStream = function (id) {
+        var _this = this;
+        return new es6_promise_1.Promise(function (resolve, reject) {
+            var gridSave = mongodb_1.MongoClient.connect(_this.connectionUrl, _this.mongoClientOptions);
+            return gridSave.then(function (client) {
+                var connection = client.db(_this.databaseName);
+                var bucket = new mongodb_1.GridFSBucket(connection, { bucketName: _this.bucketName });
+                bucket.find({ _id: new bson_1.ObjectID(id) }).toArray().then(function (result) {
+                    if (result.length > 0) {
+                        resolve(bucket.openDownloadStream(new bson_1.ObjectID(id)));
+                    }
+                    else {
+                        reject();
+                    }
+                });
+            });
+        });
+    };
+    /**
+     * Get a single Object
+     * @param {string} id
+     * @return {Promise<IGridFSObject>}
+     */
+    GridFSPromise.prototype.getObject = function (id) {
+        var _this = this;
+        return new es6_promise_1.Promise((function (resolve, reject) {
+            var gridSave = mongodb_1.MongoClient.connect(_this.connectionUrl, _this.mongoClientOptions);
+            gridSave.then(function (client) {
+                var connection = client.db(_this.databaseName);
+                var bucket = new mongodb_1.GridFSBucket(connection, { bucketName: _this.bucketName });
+                bucket.find({ _id: new bson_1.ObjectID(id) }).toArray().then(function (result) {
+                    if (result.length > 0) {
+                        resolve(result);
+                    }
+                    else {
+                        reject();
+                    }
+                });
+            });
+        }));
+    };
+    return GridFSPromise;
+}());
+exports.GridFSPromise = GridFSPromise;
---
+++ src/GridFSPromise.ts
@@ -0,0 +1,125 @@
+import * as fs from "fs";
+import {Promise} from "es6-promise";
+import {GridFSBucket, GridFSBucketReadStream, MongoClient, MongoClientOptions} from "mongodb";
+import {ObjectID} from "bson";
+
+export interface IGridFSObject {
+    _id: ObjectID;
+    fileName: string;
+    contentType: string;
+    meta: object;
+    fileSize: number;
+}
+
+export class GridFSPromise {
+
+    private databaseName: string;
+    private connectionUrl: string;
+    private basePath: string;
+    private mongoClientOptions: MongoClientOptions;
+
+    private bucketName: string;
+
+    /**
+     *
+     * @param {string} mongoUrl
+     * @param {string} databaseName
+     * @param {MongoClientOptions} mongoOptions
+     * @param {string} basePath
+     * @param {string} bucketName
+     */
+    constructor(mongoUrl: string, databaseName: string, mongoOptions: MongoClientOptions, basePath?: string, bucketName?: string) {
+
+        this.databaseName = databaseName;
+        this.connectionUrl = mongoUrl;
+        this.mongoClientOptions = mongoOptions;
+
+        this.basePath = basePath || __dirname;
+
+        this.bucketName =  bucketName || "fs";
+
+    }
+
+    /**
+     * Save the File from the GridFs to the filesystem and get the Path back
+     * @param {string} id
+     * @param {string} fileName
+     * @param filePath
+     * @return {Promise<string>}
+     */
+    public getFile(id: string, fileName: string, filePath: string = "/tmpArchive/archive/"): Promise<string> {
+
+        return new Promise((resolve, reject) => {
+
+            let gridSave = MongoClient.connect(this.connectionUrl);
+            gridSave.then((client) => {
+                let connection = client.db(this.databaseName);
+                let bucket = new GridFSBucket(connection, {bucketName: this.bucketName});
+
+                bucket.openDownloadStream(new ObjectID(id))
+                    .once("error", (error) => {
+                        reject(error);
+                    }).once("finish", () => {
+                        resolve(this.basePath.concat(`${filePath}${fileName}`));
+                    })
+                    .pipe(fs.createWriteStream(this.basePath.concat(`${filePath}${fileName}`)));
+
+            }).catch((err) => {
+                reject(err);
+            });
+
+        });
+
+    }
+
+    /**
+     * Returns a stream of a file from the GridFS.
+     * @param {string} id
+     * @return {Promise<GridFSBucketReadStream>}
+     */
+    public getFileStream(id: string): Promise<GridFSBucketReadStream> {
+
+        return new Promise<GridFSBucketReadStream>((resolve, reject) => {
+            let gridSave = MongoClient.connect(this.connectionUrl, this.mongoClientOptions);
+            return gridSave.then((client) => {
+                let connection = client.db(this.databaseName);
+                let bucket = new GridFSBucket(connection, {bucketName: this.bucketName});
+
+                bucket.find({_id: new ObjectID(id)}).toArray().then((result) => {
+                    if (result.length > 0) {
+                        resolve(bucket.openDownloadStream(new ObjectID(id)));
+                    } else {
+                        reject();
+                    }
+                });
+            });
+        });
+    }
+
+    /**
+     * Get a single Object
+     * @param {string} id
+     * @return {Promise<IGridFSObject>}
+     */
+    public getObject(id: string) {
+
+        return new Promise(((resolve, reject) => {
+            let gridSave = MongoClient.connect(this.connectionUrl, this.mongoClientOptions);
+            gridSave.then((client) => {
+                let connection = client.db(this.databaseName);
+                let bucket = new GridFSBucket(connection, {bucketName: this.bucketName});
+
+                bucket.find({_id: new ObjectID(id)}).toArray().then((result) => {
+                    if (result.length > 0) {
+                        resolve(result);
+                    } else {
+                        reject();
+                    }
+                });
+
+            });
+        }));
+
+    }
+
+}
